6b23f6fe93d8ee8123cd6e99625be9f8bed0e655
Harry <harry.lees@gmail.com>
2021-10-31 06:18:57 +0000

    removed S from Finance Friends :(

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -4,7 +4,7 @@
             <div class="relative flex items-center justify-between h-14">
 
                 <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-                    <div class="flex-shrink-0 flex items-center text-lg text-gray-800"><router-link to="/">Finance Friends</router-link></div>
+                    <div class="flex-shrink-0 flex items-center text-lg text-gray-800"><router-link to="/">Finance Friend</router-link></div>
                 </div>
 
                 <div class="flex items-center space-x-12 text-s">
